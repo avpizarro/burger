788097a2f364a93ca6735573817df49cf048a3f4
avpizarro <avpizarro@live.com>
2021-02-21 18:01:46 +1100

    Update to JAWsDB

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-USE burgers_db;
-
 INSERT INTO burgers (burger_name) VALUES ('Big Mac');
 INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
 INSERT INTO burgers (burger_name) VALUES ('Cheesegurger');
