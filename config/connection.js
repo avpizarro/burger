80dff89787a35476712558d07c177adf1fbc67dd
avpizarro <avpizarro@live.com>
2021-02-21 18:31:01 +1100

    Change connection to var

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Set up MySQL connection.
-const mysql = require("mysql");
-let connection;
+var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
